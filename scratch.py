55826e0d387e63e8eaa91bcc673fd603c1c3c968
Eric Smith <j.eric.smith@live.com>
2017-07-21 04:46:12 -0400

    additional files to assist with readme instructions

---
+++ scratch.py
@@ -0,0 +1,6 @@
+import secret_message
+
+# secret_message.message_scrambler.scramble_message('Pokemon is not a Japanese word', 'c:\secret\scrambled')
+
+
+secret_message.message_descrambler.descramble_message('c:\secret\scrambled')
